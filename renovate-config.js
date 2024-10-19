ced7f1a8803bd339156cd6103308271028a162ab
Khatib Abbas <abbas_khatib@hotmail.com>
2024-10-19 20:17:13 +0200

    Update renovate-config.js

--- renovate-config.js
+++ renovate-config.js
@@ -1,15 +1,9 @@
-module.exports = {
+{
   "packageRules": [
-    {
-      "matchDatasources": ["docker"],
-      "matchPackageNames": ["caddy"],
-      "groupName": "Caddy Server updates",
-      "enabled": true
-    }
     {
       "matchPackageNames": ["java"],
       "replacementName": "eclipse-temurin",
       "replacementVersion": "11"
     }
-  ],
-};
+  ]
+}
