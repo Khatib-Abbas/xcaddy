8ac8b0eb91af854c6a4390fe1e789205cfba7489
Khatib Abbas <abbas_khatib@hotmail.com>
2024-10-19 20:23:59 +0200

    Update renovate-config.js

--- renovate-config.js
+++ renovate-config.js
@@ -1,4 +1,7 @@
-module.exports = {
+{
+  $schema: "https://docs.renovatebot.com/renovate-schema.json",
+  platform: "github",
+  repositories: ["Khatib-Abbas/xcaddy"],
   "packageRules": [
     {
       "matchPackageNames": ["java"],
@@ -6,4 +9,4 @@ module.exports = {
       "replacementVersion": "11"
     }
   ]
-};
+}
